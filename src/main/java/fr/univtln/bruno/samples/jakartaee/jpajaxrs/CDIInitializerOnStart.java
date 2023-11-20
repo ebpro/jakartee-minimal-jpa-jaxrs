4432abe39af661b948e9429091c7122ba713ca41
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2023-11-20 17:37:42 +0100

    cleans up @Inject.

--- src/main/java/fr/univtln/bruno/samples/jakartaee/jpajaxrs/CDIInitializerOnStart.java
+++ src/main/java/fr/univtln/bruno/samples/jakartaee/jpajaxrs/CDIInitializerOnStart.java
@@ -1,26 +1,23 @@
 package fr.univtln.bruno.samples.jakartaee.jpajaxrs;
 
-import jakarta.annotation.PostConstruct;
-import jakarta.ejb.Singleton;
-import jakarta.ejb.Startup;
 import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
+import lombok.NoArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.UUID;
 
 @Slf4j
 @ApplicationScoped
+@RequiredArgsConstructor(onConstructor = @__({@Inject}))
 public class CDIInitializerOnStart {
-
-    @Inject
-    MessageDAO messageDAO;
+    private final MessageDAO messageDAO;
 
     public void onStart(@Observes @Initialized(ApplicationScoped.class) Object object) {
         log.info("Init CDI singleton");
-        messageDAO.create(Message.of(UUID.randomUUID(),"Test init massage"));
+        messageDAO.create(Message.of(UUID.randomUUID(),"Test message from initialisation singleton."));
     }
-
 }
