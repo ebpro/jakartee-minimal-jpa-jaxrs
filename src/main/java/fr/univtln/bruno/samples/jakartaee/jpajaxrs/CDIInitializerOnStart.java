03ebed143304e1389ebf46bef786cc426f8aa1a0
Emmanuel Bruno <emmanuel.bruno@univ-tln.fr>
2023-11-20 17:38:47 +0100

    cleans imports.

--- src/main/java/fr/univtln/bruno/samples/jakartaee/jpajaxrs/CDIInitializerOnStart.java
+++ src/main/java/fr/univtln/bruno/samples/jakartaee/jpajaxrs/CDIInitializerOnStart.java
@@ -4,7 +4,6 @@ import jakarta.enterprise.context.ApplicationScoped;
 import jakarta.enterprise.context.Initialized;
 import jakarta.enterprise.event.Observes;
 import jakarta.inject.Inject;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
